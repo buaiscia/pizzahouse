6170ae4583aefe43aa1245f6929cf909cb1535bc
Alex Buaiscia <alex.buaiscia@24i.com>
2021-02-14 21:20:04 +0100

    blade + routing basics

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/pizzas', function () {
+    $pizza = [
+        'type' => 'naples',
+        'base' => 'cheesy',
+        'price' => '20'
+    ];
+    return view('pizzas', $pizza);
+});
